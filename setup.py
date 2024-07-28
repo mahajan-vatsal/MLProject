6304ee5037bb885ac5b4ce75f6872bd68f0b3df8
Vatsal Mahajan <Vatsalmahajan0007@gmail.com>
2024-07-29 01:53:05 +0200

    Created requirements.txt & setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+
+name='MLProject',
+version='0.0.1',
+author='Vatsal Mahajan',
+author_email='vatsalmahajan0007@gmail.com',
+packages=find_packages(),
+install_requires=['pandas', 'numpy', 'seaborn']
+
+
+)
