1dc598cc40c76f883fa51cc33d57c75cf44c7c6e
Vatsal Mahajan <Vatsalmahajan0007@gmail.com>
2024-07-29 16:06:25 +0200

    Changes in code

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        #if HYPEN_E_DOT in requirements:
+         #   requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
@@ -25,7 +25,6 @@ version='0.0.1',
 author='Vatsal Mahajan',
 author_email='vatsalmahajan0007@gmail.com',
 packages=find_packages(),
-install_requires= get_requirements('requirements.txt')
-
+install_requires=get_requirements('requirements.txt')
 
 )
